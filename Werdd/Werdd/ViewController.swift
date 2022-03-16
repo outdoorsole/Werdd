eb18e33ef232e77ac840730f0f79b1a6f428e95b
Maribel M <outdoorsole@users.noreply.github.com>
2022-03-16 16:51:52 -0700

    Set top, leading & trailing constraints for submitButton

--- Werdd/Werdd/ViewController.swift
+++ Werdd/Werdd/ViewController.swift
@@ -40,7 +40,11 @@ class ViewController: UIViewController {
             helloLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
             helloLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
             helloLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
-            helloLabel.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5)
+            helloLabel.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5),
+
+            submitButton.topAnchor.constraint(equalTo: helloLabel.bottomAnchor, constant: 20),
+            submitButton.leadingAnchor.constraint(equalTo: helloLabel.leadingAnchor, constant: 40),
+            submitButton.trailingAnchor.constraint(equalTo: helloLabel.trailingAnchor, constant: -40)
         ])
     }
 }
