556ede69e7f4f823a098e52bc708772d30723372
Maribel M <outdoorsole@users.noreply.github.com>
2022-03-22 16:11:45 -0700

    Remove submitButton

--- Werdd/Werdd/ViewController.swift
+++ Werdd/Werdd/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let helloLabel: UILabel = {
+    let titleLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.text = "Werdd."
@@ -20,32 +20,18 @@ class ViewController: UIViewController {
         return label
     }()
 
-    let submitButton: UIButton = {
-        let button = UIButton()
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.setTitle("Submit", for: .normal)
-        button.backgroundColor = .green
-        return button
-    }()
-
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor =  UIColor(red: 0.92, green: 0.91, blue: 0.90, alpha: 1.00)
 
-        view.addSubview(helloLabel)
-        view.addSubview(submitButton)
+        view.addSubview(titleLabel)
 
         NSLayoutConstraint.activate([
-            helloLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
-            helloLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
-            helloLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
-            helloLabel.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5),
-
-            submitButton.topAnchor.constraint(equalTo: helloLabel.bottomAnchor, constant: 20),
-            submitButton.leadingAnchor.constraint(equalTo: helloLabel.leadingAnchor, constant: 40),
-            submitButton.trailingAnchor.constraint(equalTo: helloLabel.trailingAnchor, constant: -40),
-            submitButton.heightAnchor.constraint(equalToConstant: 200)
+            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+            titleLabel.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5)
         ])
     }
 }
