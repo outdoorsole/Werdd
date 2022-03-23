88b250de62332ee126cdd2790344a1fad4adc429
Maribel M <outdoorsole@users.noreply.github.com>
2022-03-22 21:22:44 -0700

    Create stackView, add labels to the stackView & add constraints

--- Werdd/Werdd/ViewController.swift
+++ Werdd/Werdd/ViewController.swift
@@ -39,19 +39,40 @@ class ViewController: UIViewController {
         return label
     }()
 
+    let stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.distribution = .fillEqually
+        stackView.spacing = 10
+        return stackView
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor =  UIColor(red: 0.92, green: 0.91, blue: 0.90, alpha: 1.00)
 
         view.addSubview(titleLabel)
-        view.addSubview(wordLabel)
-        view.addSubview(speechLabel)
-        view.addSubview(definitionLabel)
+        view.addSubview(stackView)
+
+        stackView.addArrangedSubview(wordLabel)
+        stackView.addArrangedSubview(speechLabel)
+        stackView.addArrangedSubview(definitionLabel)
+
+        stackView.backgroundColor = .darkGray
+        wordLabel.backgroundColor = .yellow
+        speechLabel.backgroundColor = .red
+        definitionLabel.backgroundColor = .blue
 
         NSLayoutConstraint.activate([
             titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 58),
-            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24)
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24),
+
+            stackView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 23),
+            stackView.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -23)
         ])
     }
 }
