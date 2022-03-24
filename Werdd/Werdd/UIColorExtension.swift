f33e7316bf3b598057e5f6c15def19ca8d7c860b
Maribel M <outdoorsole@users.noreply.github.com>
2022-03-24 12:42:13 -0700

    Move UIColor extension into separate file

---
+++ Werdd/Werdd/UIColorExtension.swift
@@ -0,0 +1,13 @@
+//
+//  UIColorExtension.swift
+//  Werdd
+//
+//  Created by m on 3/24/22.
+//
+
+import UIKit
+
+extension UIColor {
+    class var lightBeige: UIColor { UIColor(named: "lightBeige") ?? UIColor(red: 0.91, green: 0.90, blue: 0.89, alpha: 1.00) }
+    class var steelBlue: UIColor { UIColor(named: "steelBlue") ?? UIColor(red: 0.21, green: 0.51, blue: 0.72, alpha: 0.60) }
+}
--- Werdd/Werdd/ViewController.swift
+++ Werdd/Werdd/ViewController.swift
@@ -110,8 +110,3 @@ class ViewController: UIViewController {
         ])
     }
 }
-
-extension UIColor {
-    class var lightBeige: UIColor { UIColor(named: "lightBeige") ?? UIColor(red: 0.91, green: 0.90, blue: 0.89, alpha: 1.00) }
-    class var steelBlue: UIColor { UIColor(named: "steelBlue") ?? UIColor(red: 0.21, green: 0.51, blue: 0.72, alpha: 0.60) }
-}
